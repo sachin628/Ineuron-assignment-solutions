7bc93d3a9831e63c9b08b76b08c3ccb6770eb365
sachin628 <sachinsharmagi9999@gmail.com>
2022-10-01 13:40:29 +0530

    my commit

---
+++ assignment1.py
@@ -0,0 +1,53 @@
+#Write a command to get the Python version you are using.
+
+
+
+#                 python --version
+
+#2. Write a python script to print Hello Python on the screen directly in the commandline.
+print("Hello Python")
+
+
+#3. Write a python script and store it in a file and execute the file from the command line.
+#                python hello.py
+
+
+
+#4. Write a python script to print Hello Python on the screen using visual studio code.
+print('Hello Python')
+
+
+#5. Write a python script to print Hello on the first line and Python on the second line
+print('Hello \nPython')
+
+
+#6. Write a python script to print “MySirG” on the screen
+print('MySirG')
+
+
+#7. Write a python script to print “Teacher’s Day” on the screen
+print("Teaher's Day")
+
+#8. Write a python script to print the value of a variable. Variable contains your name as data
+X = 'Sachin Sharma' 
+print(X)
+
+#9. Write a python script to create variables to store your name, age, qualification, yearsof experience and print it.
+name= "Sachin sharma"
+age = 22 
+qualification = "B.Tech"
+years=  2
+print(name)
+print(age)
+print(qualification)
+print(years)
+print(name, age, qualification, years)
+
+
+#10. Write a python script to print values of three variables, each in a new line. Variable contains integer values
+x = 20
+y = 55
+z = 87
+print(x)
+print(y)
+print(z)
